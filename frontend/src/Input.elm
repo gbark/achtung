a3c99eaf2d3bde8151dbcadf45786832b05f739b
gbark <gustaf@frontengineering.com>
2016-03-02 13:40:11 +0000

    Remove old experiment

--- frontend/src/Input.elm
+++ frontend/src/Input.elm
@@ -43,23 +43,3 @@ gamearea : Signal (Int, Int)
 gamearea =
     (Signal.map (\(w, h) -> (w-Config.sidebarWidth-Config.sidebarBorderWidth, h)) Window.dimensions)
     
-    
--- convertServerInput : ServerInput -> Input
--- convertServerInput serverInput =
---     { serverInput | players = (List.map convertPlayers serverInput.players)
---     } 
-    
-    
--- convertPlayers player =
---     { player | id = 99
---              , path = (List.map convertPath player.path)
---              , color = Color.rgb 254 221 3
---              , leftKey = (Char.toCode 'O')
---              , leftKey = (Char.toCode 'P')
---              , keyDesc = "O,P"
---              }
-             
-             
--- convertPath : Map -> Position (Float, Float)
--- convertPath path =
---     Position Hidden (path.x, path.y)
